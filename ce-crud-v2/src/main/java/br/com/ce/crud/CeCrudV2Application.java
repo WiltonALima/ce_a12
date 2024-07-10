02bf10a57d6d3e5dcdecafcf55ecdd1433ac9b89
Wilton Lima <wilton.a.lima@gmail.com>
2024-07-09 22:33:24 -0300

    Adicionando configuração inicial

---
+++ ce-crud-v2/src/main/java/br/com/ce/crud/CeCrudV2Application.java
@@ -0,0 +1,13 @@
+package br.com.ce.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeCrudV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeCrudV2Application.class, args);
+	}
+
+}
---
+++ ce-crud-v2/src/test/java/br/com/ce/crud/CeCrudV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ce.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeCrudV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
